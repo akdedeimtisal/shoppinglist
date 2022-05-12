dfa5fa56aef7de98b8cf7794f1acb8457374afad
imtisal.akdede <imtisal.akdede@tubitak.gov.tr>
2022-05-12 14:27:23 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import AddItem from './AddItem';
+import Stack from '@mui/material/Stack';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const addItem = (item) => {
+    setItems([item, ...items]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+     <AppBar position='static'>
+       <ToolBar>
+         <Typography  variant="h6">
+             Shopping List
+         </Typography>
+       
+       </ToolBar>
+     </AppBar>
+     <Stack alignItems="center">
+     <AddItem addItem={addItem}/>
+     <List>
+      {
+        items.map((item, index) =>
+          <ListItem key={index} divider>
+            <ListItemText
+              primary={item.product}
+              secondary={item.amount} />
+          </ListItem>       
+        
+        )
+      }
+
+     </List>
+
+     </Stack>
+   </Container>
   );
 }
 
